0a32e823d5cca32159f9e00c3bc29c8caf1892f5
XalagonGR <124578827+Xalagon17@users.noreply.github.com>
2023-02-08 12:24:22 +0100

    Create setup.cpp

---
+++ setup.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main(){
+system("sudo apt upgrade");
+system("sudo apt update");
+system("sudo apt-get install wireshark");
+system("sudo apt-get install tcpdump");
+return 0;
+}
